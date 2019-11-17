a2bb27cb1c337401bfd8ebada04e6287d46ef17a
Rizqun <50146188+Rizqun@users.noreply.github.com>
2019-11-17 22:29:42 +0700

    Add files via upload

---
+++ BlogRizqun/application/views/news.php
@@ -0,0 +1,17 @@
+<header>
+  <h2>News</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">Hot News</a></li>
+      <li><a href="#">Older News</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>Hot News</h1>
+    <p>No news for today :(</p>
+  </article>
+</section>
---
+++ BlogRizqun/application/views/pictures.php
@@ -0,0 +1,17 @@
+<header>
+  <h2>Pictures</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">New Pictures</a></li>
+      <li><a href="#">Old Pictures</a></li>
+    </ul>
+  </nav>
+  
+  <article>
+    <h1>New Pictures</h1>
+    <p>No available picture for now :(</p>
+  </article>
+</section>
